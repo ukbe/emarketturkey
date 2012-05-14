70aee05266aff9d4c99a56d3641b79e5abd1ef19
uakdogan <uakdogan@mycompany.com>
2012-05-14 12:53:39 +0000

    added redirect action to support backwards compatibility of old company profile links
    git-svn-id: file:///svn2git/svn/repos/emarket/website/trunk@46 5bad16f7-618a-4fe3-8011-2f6d34f883f8

---
+++ apps/b2b/modules/company/actions/profileBCAction.class.php
@@ -0,0 +1,27 @@
+<?php
+
+class profileBCAction extends EmtAction
+{
+
+    public function execute($request)
+    {
+        if ($this->getRequestParameter('id') < 4739)
+        {
+            $action = $this->getRequestParameter('paction');
+            
+            $hash = myTools::flipHash($this->getRequestParameter('id'), false, PrivacyNodeTypePeer::PR_NTYP_COMPANY);
+
+            $arr = array('profile'      => "@company-profile?hash=$hash",
+                         'event'        => "@company-profile-action?hash=$hash&action=connections&relation=friend",
+                         'partners'     => "@company-profile-action?hash=$hash&action=connections&relation=friend",
+                         'network'      => "@company-profile-action?hash=$hash&action=connections&relation=friend",
+                         'people'       => "@company-profile-action?hash=$hash&action=connections&relation=friend",
+                         'photos'       => "@company-profile-action?hash=$hash&action=connections&relation=friend",
+                         'products'     => "@company-profile-action?hash=$hash&action=connections&relation=friend"
+                    );
+
+            $this->redirect($action ? $arr[$action] : "@user-profile?hash=$hash", 301);
+        }
+    }
+
+}
