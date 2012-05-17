c502d81bdff681eb8e47338843e9e83b51771cf7
uakdogan <uakdogan@mycompany.com>
2012-05-17 12:46:21 +0000

    updated error404 page
    git-svn-id: file:///svn2git/svn/repos/emarket/website/trunk@89 5bad16f7-618a-4fe3-8011-2f6d34f883f8

--- apps/lobby/modules/default/templates/error404Success.php
+++ apps/lobby/modules/default/templates/error404Success.php
@@ -1,5 +1,20 @@
-<h2><?php echo __('Oops! We couldn\'t find requested page!') ?></h2>
-<ul>
-<li><?php echo __('Please make sure your typed the correct url in the address bar.') ?></li>
-<li><?php echo __('Content which was included in the page you requested might be outdated and removed.') ?></li>
-</ul>
+<div class="col_948">
+
+    <div class="col_180">
+<?php include_partial('leftmenu')?>
+    </div>
+
+    <div class="col_576">
+        <div class="box_576 noBorder _noBorder">
+
+            <h4><?php echo __('Page Not Found!') ?></h4>
+
+            <h2><?php echo __("Oops! We couldn't find requested page!") ?></h2>
+            <ul>
+                <li><?php echo __('Please make sure your typed the correct url in the address bar.') ?></li>
+                <li><?php echo __('Content which was included in the page you requested might be outdated and removed.') ?></li>
+            </ul>
+        </div>
+    </div>
+
+</div>
