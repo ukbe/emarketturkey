8c3506c247150b6a6e0bb61a1e144ff261434186
uakdogan <uakdogan@mycompany.com>
2012-05-19 18:04:28 +0000

    added search-users route, moved old search/results action to default/searchBC
    git-svn-id: file:///svn2git/svn/repos/emarket/website/trunk@116 5bad16f7-618a-4fe3-8011-2f6d34f883f8

---
+++ apps/cm/modules/default/actions/searchBCAction.class.php
@@ -0,0 +1,26 @@
+<?php
+
+class searchBCAction extends EmtAction
+{
+    public function execute($request)
+    {
+        $within = myTools::pick_from_list($this->getRequestParameter('within'), array(PrivacyNodeTypePeer::PR_NTYP_USER, PrivacyNodeTypePeer::PR_NTYP_GROUP), null);
+
+        $criteria['keyword'] = $this->getRequestParameter('keyword');
+        $criteria['location'] = $this->getRequestParameter('location');
+        if ($within == PrivacyNodeTypePeer::PR_NTYP_GROUP)
+            $criteria['type'] = $this->getRequestParameter('type');
+
+        $criteria = array_filter($criteria);
+        
+        $criteria = http_build_query($criteria);
+
+        if ($within == PrivacyNodeTypePeer::PR_NTYP_GROUP)
+            $this->redirect('@search-groups' . ($criteria ? "?$criteria" : ''), 301);
+        elseif ($within == PrivacyNodeTypePeer::PR_NTYP_USER)
+            $this->redirect('@search-users' . ($criteria ? "?$criteria" : ''), 301);
+        else
+            $this->redirect404();
+    }
+    
+}
