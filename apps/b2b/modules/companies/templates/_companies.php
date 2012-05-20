629e24e03226485da1224dad628795d6e9e5f260
uakdogan <uakdogan@mycompany.com>
2012-05-20 11:54:28 +0000

    added Search Results item to left menu
    git-svn-id: file:///svn2git/svn/repos/emarket/website/trunk@131 5bad16f7-618a-4fe3-8011-2f6d34f883f8

--- apps/b2b/modules/companies/templates/_companies.php
+++ apps/b2b/modules/companies/templates/_companies.php
@@ -1,7 +1,11 @@
 <?php $action = sfContext::getInstance()->getActionName() ?>
             <ul class="_side">
                 <li<?php echo $action=='featured' ? ' class="_on"' :'' ?>><?php echo link_to(__('Featured Companies'), "@companies-action?action=featured") ?>
-                <li<?php echo $action=='index' ? ' class="_on"' :'' ?>><?php echo link_to(__('Find a Company'), "@companies") ?></li>
+                <li<?php echo $action=='index' ? ' class="_on"' :'' ?>><?php echo link_to(__('Find a Company'), "@companies") ?>
+                    <?php if (($action=='directory' && (isset($mod) && $mod==1))): ?>
+                    <ul><li class="_on"><?php echo link_to(__('Search Results'), '@companies') ?></li></ul>
+                    <?php endif ?>
+                    </li>
                 <li<?php echo $action=='connected' ? ' class="_on"' :'' ?>><?php echo link_to(__('Companies in Your Network'), "@companies-action?action=connected") ?>
                 <li<?php echo ($action=='byIndustry' || (isset($mod) && $mod==1)) ? ' class="_on"' :'' ?>><?php echo link_to(__('Browse by Industry'), "@companies-action?action=byIndustry") ?></li>
                 <li<?php echo ($action=='byCountry' || (isset($mod) && $mod==2)) ? ' class="_on"' :'' ?>><?php echo link_to(__('Browse by Country'), "@companies-action?action=byCountry") ?></li>
--- apps/b2b/modules/products/templates/_products.php
+++ apps/b2b/modules/products/templates/_products.php
@@ -2,7 +2,11 @@
         <div class="box_180">
             <ul class="_side">
                 <li<?php echo $action=='spotlight' ? ' class="_on"' :'' ?>><?php echo link_to(__('Products Spotlight'), "@products-action?action=spotlight") ?>
-                <li<?php echo $action=='index' ? ' class="_on"' :'' ?>><?php echo link_to(__('Find a Product'), "@products") ?></li>
+                <li<?php echo $action=='index' ? ' class="_on"' :'' ?>><?php echo link_to(__('Find a Product'), "@products") ?>
+                    <?php if (($action=='directory' && (isset($mod) && $mod==1))): ?>
+                    <ul><li class="_on"><?php echo link_to(__('Search Results'), '@products') ?></li></ul>
+                    <?php endif ?>
+                    </li>
                 <li<?php echo $action=='network' ? ' class="_on"' :'' ?>><?php echo link_to(__('Products in Your Network'), "@products-action?action=network") ?>
                 <li<?php echo ($action=='byCategory' || (isset($mod) && $mod==1)) ? ' class="_on"' :'' ?>><?php echo link_to(__('Browse by Category'), "@products-action?action=byCategory") ?></li>
                 <li<?php echo ($action=='byCountry' || (isset($mod) && $mod==2)) ? ' class="_on"' :'' ?>><?php echo link_to(__('Browse by Country'), "@products-action?action=byCountry") ?></li>
