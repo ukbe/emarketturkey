58191106e3f27c3014137a2c7c90ccaa254c164d
uakdogan <uakdogan@mycompany.com>
2012-05-28 09:28:58 +0000

    added missing functionality for footer lang select
    git-svn-id: file:///svn2git/svn/repos/emarket/website/trunk@157 5bad16f7-618a-4fe3-8011-2f6d34f883f8

--- apps/cm/templates/_island-footer.php
+++ apps/cm/templates/_island-footer.php
@@ -6,8 +6,8 @@
                     <li><?php echo link_to(__('For Individuals'), '@lobby.for-individuals') ?></li>
                     <li><?php echo link_to(__('Privacy Policy'), '@lobby.privacy') ?></li>
                     <li><?php echo link_to(__('Terms of Use'), '@lobby.terms') ?></li>
-                    <li><? echo form_tag($sf_request->getUri()) ?>
-                        <?php echo select_tag('lang', options_for_select(array('tr' => 'Türkçe', 'en' => 'English'), $sf_user->getCulture()), 'class=ui-corner-all onchange=submit();') ?>
+                    <li><?php echo form_tag($sf_request->getUri(), 'method=GET') ?>
+                        <?php echo select_tag('x-cult', options_for_select(array('en' => 'English', 'tr' => 'Türkçe'), $sf_user->getCulture()), array('onchange' => "$(this).closest('form').submit();")) ?>
                         </form></li>
                     <li class="_right"><?php echo __('All rights reserved') ?>&nbsp;&nbsp;EMTPORT A.Ş.</li>
 				</ul>
