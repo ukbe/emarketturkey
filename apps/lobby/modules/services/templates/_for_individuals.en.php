c2bc076b5e3e1bddfbd0c2bc794772dceba5465b
uakdogan <uakdogan@mycompany.com>
2012-05-28 15:12:43 +0000

    added css list-style-type property
    git-svn-id: file:///svn2git/svn/repos/emarket/website/trunk@159 5bad16f7-618a-4fe3-8011-2f6d34f883f8

--- apps/lobby/modules/services/templates/_for_individuals.en.php
+++ apps/lobby/modules/services/templates/_for_individuals.en.php
@@ -1,7 +1,7 @@
 <div>
     <p>eMarketTurkey; a social platform that is extra-ordinary, innovative and far beyond from the usual social platforms, gives you the chance to become a member or create social and business networks. When you’re capable of such things, you can share news, bulletins invitations with the groups that you’re a member of, plus you can jump to different groups and networks easily.</p>
     <p>Additionally, forget that on eMarketTurkey you can professionally get it touch with empoyers and experts from all kinds of sectors, follow the trends in business and finance, easily reach lots of useful information, researches, surveys and academic articles with the help of “B2B”, “Career” and “Academy” sections on eMarketTurkey.</p>
-    <ul>
+    <ul style="list-style-image: url(/images/layout/icon/right-arrow.png);">
         <li>Become a member of social and business networks</li>
         <li>Create your own group,</li>
         <li>Share your thoughts,</li>
