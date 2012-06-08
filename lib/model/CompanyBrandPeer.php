246d6b2f561f6eaf5aed40bb7c2e2efaf7a4c566
uakdogan <uakdogan@mycompany.com>
2012-06-08 12:39:05 +0000

    added no brand constant
    git-svn-id: file:///svn2git/svn/repos/emarket/website/trunk@200 5bad16f7-618a-4fe3-8011-2f6d34f883f8

--- lib/model/CompanyBrandPeer.php
+++ lib/model/CompanyBrandPeer.php
@@ -2,11 +2,13 @@
 
 class CompanyBrandPeer extends BaseCompanyBrandPeer
 {
+    CONST BRND_NONE                        = 0;
     CONST BRND_HOLDED_BY_COMPANY           = 1;
     CONST BRND_HOLDED_BY_ELSE              = 2;
     
     
-    public static $typeNames    = array (1 => 'Owned by Our Company',
+    public static $typeNames    = array (0 => 'Not Branded',
+                                         1 => 'Owned by Our Company',
                                          2 => 'Owned by Someone Else',
                                   );
 }
