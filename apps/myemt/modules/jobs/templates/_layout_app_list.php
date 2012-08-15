e8daf2ea01d8e9e1130fdc610d7c6aea3f5fe006
uakdogan <uakdogan@mycompany.com>
2012-08-15 22:24:49 +0000

    bugfix (missing resume causing fatal error)
    git-svn-id: file:///svn2git/svn/repos/emarket/website/trunk@219 5bad16f7-618a-4fe3-8011-2f6d34f883f8

--- apps/myemt/modules/jobs/templates/_layout_app_list.php
+++ apps/myemt/modules/jobs/templates/_layout_app_list.php
@@ -12,7 +12,7 @@
 <?php if (count($results = $pager->getResults())): ?>
 <?php foreach ($results as $app): ?>
     <tr<?php echo !$app->getEmployerRead() ? ' class="unread"' : '' ?>>
-        <td><?php echo link_to(image_tag($app->getUser()->getResume()->getPhotoUri()), $url = $job->getApplicantUrl($app->getId())) ?></td>
+        <td><?php echo link_to(image_tag($app->getUser()->getResume() ? $app->getUser()->getResume()->getPhotoUri() : $app->getUser()->getProfilePictureUri()), $url = $job->getApplicantUrl($app->getId())) ?></td>
         <td><?php echo $app->getFlagType() ? image_tag(UserJobPeer::$flagTypeIcons[$app->getFlagType()], array('title' => UserJobPeer::$flagTypeNames[$app->getFlagType()])) : '' ?></td>
         <td><?php echo link_to($app->getUser(), $url) ?></td>
         <td><?php echo link_to(__(UserJobPeer::$statTypeNames[$app->getEmployerRead() ? UserJobPeer::UJ_STAT_TYP_READ : UserJobPeer::UJ_STAT_TYP_UNREAD]), $url) ?></td>
