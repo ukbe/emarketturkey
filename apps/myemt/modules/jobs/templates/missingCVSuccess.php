4f1e1a4c39b6f6cce732edd0ff6e19429116acab
uakdogan <uakdogan@mycompany.com>
2012-08-15 22:41:21 +0000

    bugfix
    git-svn-id: file:///svn2git/svn/repos/emarket/website/trunk@221 5bad16f7-618a-4fe3-8011-2f6d34f883f8

--- apps/myemt/modules/jobs/templates/missingCVSuccess.php
+++ apps/myemt/modules/jobs/templates/missingCVSuccess.php
@@ -48,7 +48,6 @@
 <?php echo link_to(__('Export'), 'mycareer/print', 'class=act a16px pdf') ?>
 </div>
 <div class="hrsplit-2"></div>
-<?php $photo = $resume->getPhoto() ?>
 <table class="cvcard">
 <tr>
     <td class="col1"><?php echo image_tag($app->getUser()->getProfilePictureUri()) ?></td>
