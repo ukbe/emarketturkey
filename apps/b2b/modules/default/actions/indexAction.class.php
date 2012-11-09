1776d08cd64e87cef86e25c03ccea80a5ab587f8
uakdogan <uakdogan@mycompany.com>
2012-11-09 13:07:05 +0000

    added more buying and selling leads
    git-svn-id: file:///svn2git/svn/repos/emarket/website/trunk@288 5bad16f7-618a-4fe3-8011-2f6d34f883f8

--- apps/b2b/modules/default/actions/indexAction.class.php
+++ apps/b2b/modules/default/actions/indexAction.class.php
@@ -22,8 +22,8 @@ class indexAction extends EmtAction
         
         $this->featured_companies = CompanyPeer::getFeaturedCompanies();
         
-        $this->selling_leads = B2bLeadPeer::getFeaturedLeads(B2bLeadPeer::B2B_LEAD_SELLING, 5);
-        $this->buying_leads = B2bLeadPeer::getFeaturedLeads(B2bLeadPeer::B2B_LEAD_BUYING, 5);
+        $this->selling_leads = B2bLeadPeer::getFeaturedLeads(B2bLeadPeer::B2B_LEAD_SELLING, 20);
+        $this->buying_leads = B2bLeadPeer::getFeaturedLeads(B2bLeadPeer::B2B_LEAD_BUYING, 20);
 
         $this->featured_shows = EventPeer::getFeaturedEvents(3, EventTypePeer::ECLS_TYP_BUSINESS);
         $this->featured_experts = TradeExpertPeer::getFeaturedTradeExperts(4);
