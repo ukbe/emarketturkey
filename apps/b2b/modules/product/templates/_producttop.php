5b9f6fff5826416deb1d1ce2411956d014600aa6
uakdogan <uakdogan@mycompany.com>
2012-11-16 13:07:28 +0000

    added __toString() function call for object print outs to prevent bad gateway error
    git-svn-id: file:///svn2git/svn/repos/emarket/website/trunk@301 5bad16f7-618a-4fe3-8011-2f6d34f883f8

--- apps/b2b/modules/product/templates/_producttop.php
+++ apps/b2b/modules/product/templates/_producttop.php
@@ -1,3 +1,3 @@
 <div class="parentlink">
-<?php echo link_to(__('Products'), '@products') . image_tag('layout/icon/bullet2.png') . '<span>' . $product . '</span>' ?>
+<?php echo link_to(__('Products'), '@products') . image_tag('layout/icon/bullet2.png') . '<span>' . $product->__toString() . '</span>' ?>
 </div>
--- apps/b2b/modules/product/templates/viewSuccess.php
+++ apps/b2b/modules/product/templates/viewSuccess.php
@@ -8,7 +8,7 @@
             <?php else: ?>
             <li><?php echo link_to(__('Selling Leads'), '@selling-leads') ?></li>
             <?php endif ?>
-            <li><?php echo link_to($lead->getProductCategory(), "@leads-dir?type_code={$lead->getTypeCode()}&substitute={$lead->getProductCategory()->getStrippedCategory()}") ?></li>
+            <li><?php echo link_to($lead->getProductCategory()->__toString(), "@leads-dir?type_code={$lead->getTypeCode()}&substitute={$lead->getProductCategory()->getStrippedCategory()}") ?></li>
             <li><span><?php echo $lead ?></span></li>
         </ul>
     </div>
@@ -55,7 +55,7 @@
                         <tr><th><?php echo __('Lead Type') ?></th>
                             <td><?php echo __(B2bLeadPeer::$typeNames[$lead->getTypeId()]) ?></td></tr>
                         <tr><th><?php echo __('Category') ?></th>
-                            <td><?php echo link_to($lead->getProductCategory(), "@leads-dir?type_code={$lead->getTypeCode()}&substitute={$lead->getProductCategory()->getStrippedCategory()}") ?></td></tr>
+                            <td><?php echo link_to($lead->getProductCategory()->__toString(), "@leads-dir?type_code={$lead->getTypeCode()}&substitute={$lead->getProductCategory()->getStrippedCategory()}") ?></td></tr>
                     </table>
                 </div>
                 <div>
@@ -64,7 +64,7 @@
                         <dt><?php echo emt_label_for('none', __('Payment Terms')) ?></dt>
                         <dd class="two_columns" style="width: 400px;">
                             <?php foreach ($payment_terms as $term): ?>
-                            <div><?php echo $term ?></div>
+                            <div><?php echo $term->__toString() ?></div>
                             <?php endforeach?>
                         </dd>
                     </dl>
@@ -101,11 +101,11 @@
             <h3><?php echo $lead->getTypeId() == B2bLeadPeer::B2B_LEAD_BUYING ? __('Buyer Information') : __('Supplier Information') ?></h3>
             <div class="t_smaller">
                 <div class="txtCenter"><?php echo $company->getLogo() ? link_to(image_tag($company->getLogo()->getThumbnailUri()), $company->getProfileUrl()) : '' ?></div>
-                <h4 class="txtCenter"><?php echo $company ?></h4>
+                <h4 class="txtCenter"><?php echo $company->__toString() ?></h4>
                 <strong><?php echo __('Industry') ?>:</strong>
-                <div class="t_smaller t_grey"><?php echo $company->getBusinessSector() ?></div>
+                <div class="t_smaller t_grey"><?php echo $company->getBusinessSector()->__toString() ?></div>
                 <strong><?php echo __('Business Type') ?>:</strong>
-                <div class="t_smaller t_grey"><?php echo $company->getBusinessType() ?></div>
+                <div class="t_smaller t_grey"><?php echo $company->getBusinessType()->__toString() ?></div>
                 <strong><?php echo __('Location') ?>:</strong>
                 <div class="t_smaller t_grey"><?php echo $company->getLocationLabel() ?></div>
                 <p><?php echo link_to(__('Go to Company Profile'), $company->getProfileUrl(), 'class=bluelink hover')?></p>
