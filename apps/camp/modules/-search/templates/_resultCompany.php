161b0e3fe3e696a9d12788a156db752ef49a9ee9
uakdogan <uakdogan@mycompany.com>
2013-01-05 23:43:36 +0000

    removing temporary module
    git-svn-id: file:///svn2git/svn/repos/emarket/website/trunk@347 5bad16f7-618a-4fe3-8011-2f6d34f883f8

--- apps/camp/modules/-search/templates/_resultCompany.php
+++
@@ -1,9 +0,0 @@
-<td class="piccol">
-    <?php echo link_to(image_tag($result->getProfilePictureUri()), $result->getProfileUrl()) ?></td>
-<td>
-<?php $contact = $result->getContact();
-      $work_address = ($contact?$contact->getWorkAddress():new ContactAddress()); ?>
-    <span style="float: right;"><?php echo $work_address->getCountry()!=''?format_country($work_address->getCountry()).'&nbsp;'.image_tag('layout/flag/'.$work_address->getCountry().'.png', 'style=margin-right:5px;width:20px;'):'' ?></span>
-    <?php echo link_to($result->getName(), $result->getProfileUrl()) ?><br />
-    <b><?php echo $result->getBusinessType() ?></b><br />
-    <em><?php echo $result->getBusinessSector() ?></em></td>
