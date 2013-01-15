a04415bb217d684afc8c0e0ba0b9acd127f98d49
uakdogan <uakdogan@mycompany.com>
2013-01-15 13:07:03 +0000

    bugfix
    git-svn-id: file:///svn2git/svn/repos/emarket/website/trunk@378 5bad16f7-618a-4fe3-8011-2f6d34f883f8

--- apps/camp/modules/signup/actions/indexAction.class.php
+++ apps/camp/modules/signup/actions/indexAction.class.php
@@ -5,8 +5,8 @@ class indexAction extends EmtAction
     public function execute($request)
     {
         $params = array();
-        $params[] = ($this->hasRequestParameter('invite') ? "invite={$this->getRequestParameter('invite')}" : "");
-        $params[] = ($this->hasRequestParameter('keepon') ? "keepon={$this->getRequestParameter('keepon')}" : "");
+        if ($this->hasRequestParameter('invite')) $params[] = "invite={$this->getRequestParameter('invite')}";
+        if ($this->hasRequestParameter('keepon')) $params[] = "keepon={$this->getRequestParameter('keepon')}";
         $this->redirect('@myemt.signup' . (count($params) ? '?' . implode('&', $params) : ''));
     }
     
