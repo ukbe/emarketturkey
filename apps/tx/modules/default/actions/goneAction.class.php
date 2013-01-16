b9697849e31f03fbcce62cac3611825ce9643227
uakdogan <uakdogan@mycompany.com>
2013-01-16 22:37:43 +0000

    added gone action for removing old and wrong page indexes added trash route for "*/_LINK_" links
    git-svn-id: file:///svn2git/svn/repos/emarket/website/trunk@410 5bad16f7-618a-4fe3-8011-2f6d34f883f8

---
+++ apps/tx/modules/default/actions/goneAction.class.php
@@ -0,0 +1,10 @@
+<?php
+
+class goneAction extends EmtAction
+{
+    public function execute($request)
+    {
+		return $this->renderText('HTTP 410 - Content has been permanently removed.<br /><br /><a href="http://www.emarketturkey.com">eMarketTurkey Home</a>');
+    }
+
+}
