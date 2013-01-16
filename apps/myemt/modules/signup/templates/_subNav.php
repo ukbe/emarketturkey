182a916d4d3823992b48a444091488d90b19e236
uakdogan <uakdogan@mycompany.com>
2013-01-16 11:04:02 +0000

    fixed links to lobby app homepage
    git-svn-id: file:///svn2git/svn/repos/emarket/website/trunk@391 5bad16f7-618a-4fe3-8011-2f6d34f883f8

--- apps/myemt/modules/signup/templates/_subNav.php
+++ apps/myemt/modules/signup/templates/_subNav.php
@@ -1,7 +1,7 @@
     <header>
         <table>
             <tr>
-                <td class="logo"><?php echo link_to(image_tag('emtlogo.gif', 'style=width:160px;'), '@lobby.homepage') ?></td>
+                <td class="logo"><?php echo link_to(image_tag('emtlogo.gif', 'style=width:160px;'), '@camp.homepage') ?></td>
                 <td><span><?php echo __('Already a member? %1sginlink - %2fpsslink', array('%1sginlink' => link_to(__('Sign in here'), '@login', 'class=inherit-font bluelink hover'),
                                                                                    '%2fpsslink' => link_to(__('Forgot Password?'), '@forgot-password', 'class=inherit-font bluelink hover'))) ?></span></td>
             </tr>
