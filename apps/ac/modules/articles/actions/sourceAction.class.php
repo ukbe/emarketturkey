f0fb3821681306ba900c339ea4b732c773901d9b
uakdogan <uakdogan@mycompany.com>
2013-01-16 20:19:02 +0000

    added articles by source and news by source pages for redirecting old indexed pages to new location
    git-svn-id: file:///svn2git/svn/repos/emarket/website/trunk@402 5bad16f7-618a-4fe3-8011-2f6d34f883f8

---
+++ apps/ac/modules/articles/actions/sourceAction.class.php
@@ -0,0 +1,18 @@
+<?php
+
+class sourceAction extends EmtAction
+{
+    public function execute($request)
+    {
+        $this->source = PublicationSourcePeer::retrieveByStrippedName($this->getRequestParameter('stripped_display_name'), true);
+
+        if (!$this->source) $this->redirect404();
+
+        $this->redirect($this->source->getUrl(PublicationPeer::PUB_TYP_ARTICLE), 301);
+    }
+
+    public function handleError()
+    {
+    }
+
+}
