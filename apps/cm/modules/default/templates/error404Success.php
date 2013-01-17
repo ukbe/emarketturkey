23842bde1bea51eded4b077cc05693922e8ed01b
uakdogan <uakdogan@mycompany.com>
2013-01-17 13:23:52 +0000

    added missing 404 pages
    git-svn-id: file:///svn2git/svn/repos/emarket/website/trunk@417 5bad16f7-618a-4fe3-8011-2f6d34f883f8

---
+++ apps/cm/modules/default/templates/error404Success.php
@@ -0,0 +1,19 @@
+<div class="col_948">
+
+    <div class="col_180">
+    &nbsp;
+    </div>
+
+    <div class="col_576">
+        <div class="box_576 noBorder _noBorder">
+
+            <h4><?php echo __('Page Not Found!') ?></h4>
+
+            <ul>
+                <li><?php echo __('Please make sure you typed the correct url in the address bar.') ?></li>
+                <li><?php echo __('Content which was included in the page you requested might be outdated and removed.') ?></li>
+            </ul>
+        </div>
+    </div>
+
+</div>
