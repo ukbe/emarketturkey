0d490472181ac1093b902d1a6a31f4297f1997ab
uakdogan <uakdogan@mycompany.com>
2013-01-24 21:46:08 +0000

    added group jobs BC action
    git-svn-id: file:///svn2git/svn/repos/emarket/website/trunk@429 5bad16f7-618a-4fe3-8011-2f6d34f883f8

---
+++ apps/cm/modules/group/actions/jobsAction.class.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * index action.
+ *
+ * @package b2b
+ * @sub-package login
+ */
+class jobsAction extends EmtGroupAction
+{
+    public function execute($request)
+    {
+        // Redirect to camp application
+        $this->redirect("@camp.group-jobs?hash={$this->group->getHash()}", 301);
+    }
+    
+}
