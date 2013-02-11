ec1744feaa4f95350a916318358fea47add43b6b
uakdogan <uakdogan@mycompany.com>
2013-02-11 22:56:25 +0000

    added i18n import php file for importing xml translations
    git-svn-id: file:///svn2git/svn/repos/emarket/website/trunk@451 5bad16f7-618a-4fe3-8011-2f6d34f883f8

--- web/importi18nsxml.php
+++ web/importi18nsxml.php
@@ -1,4 +1,7 @@
 <?php
+set_time_limit(999999);
+//ini_set('memory_limit', '2048M');
+
     $filename = $_GET['file'];
     if($filename == '')
     {
@@ -12,7 +15,7 @@
         die;
     }
 
-    $con = @oci_connect('geek', 'geeker', 'emtbag');
+    $con = oci_connect('geek', 'geeker', 'emtbag');
     
     $sql = "select cat_id from emt_tx_catalogue where target_lang='$targetln'";
     
@@ -22,87 +25,114 @@
     if ($row = oci_fetch_row($stmt))
     {
         $catid = $row[0];
+echo "found target language info<br />";
+ob_flush();
     }
     else
     {
         echo "target language not found!";
+ob_flush();
         oci_close($con);
         die;
     }
 
-
-    
-    libxml_use_internal_errors(true);
-    if (!$xml = simplexml_load_file($filename))
-    {
-      $error = false;
-
-      return $error;
-    }
-    libxml_use_internal_errors(false);
-
-    $translationUnit = $xml->xpath('//trans');
-
-    $translations = array();
-    
-    $sql = "select msg_id, source, translated from emt_tx_trans_unit where cat_id=$catid";
-    
-    $stmt = oci_parse($con, $sql);
-    oci_execute($stmt);
-
     $trans = array();
+    // Check if data already exist
+    $sql = "select msg_id, source, translated from emt_tx_trans_unit where cat_id=:catid";
+        
+    $stmt = oci_parse($con, $sql);
+    oci_bind_by_name($stmt, ':catid', $catid);
+    oci_execute($stmt);
     
     while ($row = oci_fetch_row($stmt))
     {
-        $source = (string)$row[1];
-        $trans[$source][] = (int)$row[0];
-        $trans[$source][] = (int)$row[2];
+        $src = is_object($row[1]) ? (string)$row[1]->load() : (string)$row[1];
+        $trans[$src][] = (int)$row[0];
+        $trans[$src][] = (int)$row[2];
     }
+echo "populated current translations(".count($trans).")<br />";
+ob_flush();
+
+    libxml_use_internal_errors(true);
     
+    if (file_exists($filename)) echo "found xml file.<br />";
+        else "could not find xml file.<br />";
+ob_flush();
+    if (!$xml = simplexml_load_file($filename))
+    {
+        echo "Failed loading XML\n";
+        foreach(libxml_get_errors() as $error) {
+            echo "\t", $error->message;
+        }
+        die;
+    }
+    libxml_use_internal_errors(false);
+
+echo "loaded xml content<br />";
+ob_flush();
+    
+    $translationUnit = $xml->xpath('//trans');
+
     $translated = 0;
     $inserted = 0;
+    $author = 'amangeldi';
+
+echo "parsed xml items<br />";
+ob_flush();
     
     foreach ($translationUnit as $unit)
     {
       $source = (string) $unit->source;
-      $msgid = (int) $unit->msgid;
+      $msgid = null;
+  
+
       if (!isset($trans[$source]))
       {
           $inserted++;
           
           $sql = "INSERT INTO emt_tx_trans_unit
-            (msg_id, cat_id, id, source, target, date_added, date_modified, translated) VALUES
-            ($msgid, $catid, (select count(*) from emt_tx_trans_unit), :message, :target, sysdate, sysdate, :translated)";
-            
+            (msg_id, cat_id, id, source, target, date_added, date_modified, author, translated) VALUES
+            (EMT_TX_TRANS_UNIT_SEQ.nextval, $catid, 1, :message, :target, sysdate, sysdate, :author, :translated)
+            RETURNING msg_id into :msgid";
+
           $stmt = oci_parse($con, $sql);
     
           $target = (string)$unit->target;
-          $source = (string)$unit->source;
           $translated = $target != '' ? 1 : 0;
           
           oci_bind_by_name($stmt, ':message', $source);
           oci_bind_by_name($stmt, ':target', $target);
-          
+          oci_bind_by_name($stmt, ':author', $author);
           oci_bind_by_name($stmt, ':translated', $translated);
-          oci_execute($stmt);
+          oci_bind_by_name($stmt, ':msgid', $msgid);
           
+          $result = oci_execute($stmt);
+
           $trans[$source][] = '';
           $trans[$source][1] = 1;
+if ($result) echo "inserted translation record(msgid=$msgid)<br />";
+else echo "error while inserting record(msgid=$msgid)<br />";
+ob_flush();
       }
       elseif (!$trans[$source][1])
       {
+          $msgid = $trans[$source][0];
           $translated++;
-          $sql = "update emt_tx_trans_unit set target=:target, translated=1, date_modified=sysdate where msg_id=:msg_id";
+          $sql = "update emt_tx_trans_unit set target=:target, translated=1, date_modified=sysdate where msg_id=:msgid";
           $stmt = oci_parse($con, $sql);
           $target = (string)$unit->target;
           oci_bind_by_name($stmt, ':target', $target);
-          oci_bind_by_name($stmt, ':msg_id', $trans[$source][0]);
-          oci_execute($stmt);
-          
+          oci_bind_by_name($stmt, ':msgid', $msgid);
+          $result = oci_execute($stmt);
+
           $trans[$source][1] = 1;
+if ($result) echo "updated translation record(msgid=$msgid)<br />";
+else echo "error while updating record(msgid=$msgid)<br />";
+ob_flush();
       }
     }
     
     echo "Translated: $translated - Inserted: $inserted";
+    ob_flush();
     
     oci_close($con);
