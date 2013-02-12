8442d98789458b7b7b6384ca55a9298271b344bf
uakdogan <uakdogan@mycompany.com>
2013-02-12 12:54:52 +0000

    fixed russian link
    git-svn-id: file:///svn2git/svn/repos/emarket/website/trunk@461 5bad16f7-618a-4fe3-8011-2f6d34f883f8

--- apps/myemt/templates/_island-footer.php
+++ apps/myemt/templates/_island-footer.php
@@ -7,7 +7,7 @@
                     <li><?php echo link_to(__('Privacy Policy'), '@camp.privacy') ?></li>
                     <li><?php echo link_to(__('Terms of Use'), '@camp.terms') ?></li>
                     <li><?php echo form_tag($sf_request->getUri(), 'method=GET') ?>
-                        <?php echo select_tag('x-cult', options_for_select(array('en' => 'English', 'tr' => 'Türkçe'), $sf_user->getCulture()), array('onchange' => "$(this).closest('form').submit();")) ?>
+                        <?php echo select_tag('x-cult', options_for_select(array('en' => 'English', 'tr' => 'Türkçe', 'ru' => 'РУССКИЙ'), $sf_user->getCulture()), array('onchange' => "$(this).closest('form').submit();")) ?>
                         </form></li>
                     <li class="_right"><?php echo __('All rights reserved') ?>&nbsp;&nbsp;EMTPORT A.Ş.</li>
 				</ul>
