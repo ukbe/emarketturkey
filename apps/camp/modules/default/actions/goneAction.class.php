7cc88d5ee0b279fdc545ba0d2ad6edb7e552f4e5
uakdogan <uakdogan@mycompany.com>
2013-02-13 10:50:14 +0000

    removed temporary "importi18n" route and fixed bug in "gone" action
    git-svn-id: file:///svn2git/svn/repos/emarket/website/trunk@477 5bad16f7-618a-4fe3-8011-2f6d34f883f8

--- apps/camp/modules/default/actions/goneAction.class.php
+++ apps/camp/modules/default/actions/goneAction.class.php
@@ -5,7 +5,7 @@ class goneAction extends EmtAction
     public function execute($request)
     {
         $this->getResponse()->setStatusCode(410);
-        return $this->renderPartial('HTTP 410 - Content has been permanently removed.<br /><br /><a href="http://www.emarketturkey.com">eMarketTurkey Home</a>');
+        return $this->renderText('HTTP 410 - Content has been permanently removed.<br /><br /><a href="http://www.emarketturkey.com">eMarketTurkey Home</a>');
     }
 
 }
