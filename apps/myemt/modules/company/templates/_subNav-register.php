884e1151907f2cff0c6a7aec84bb2fd2fe4275c1
uakdogan <uakdogan@mycompany.com>
2013-02-19 14:34:26 +0000

    fixed lobby link
    git-svn-id: file:///svn2git/svn/repos/emarket/website/trunk@507 5bad16f7-618a-4fe3-8011-2f6d34f883f8

--- apps/myemt/modules/company/templates/_subNav-register.php
+++ apps/myemt/modules/company/templates/_subNav-register.php
@@ -1,7 +1,7 @@
     <header>
         <table>
             <tr>
-                <td class="logo"><?php echo link_to(image_tag('emtlogo.gif', 'style=width:160px;'), '@lobby.homepage') ?></td>
+                <td class="logo"><?php echo link_to(image_tag('emtlogo.gif', 'style=width:160px;'), '@camp.homepage') ?></td>
                 <td><span><?php echo __('Register Your Company') ?></span>
                     <p style="font-weight: normal; font-size: 12px; margin: 6px 0px -30px;"><?php echo __('Please fill in the form below.') ?></p></td>
             </tr>
