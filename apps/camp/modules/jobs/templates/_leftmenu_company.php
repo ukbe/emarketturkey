d2d63acfbb8b8f1a7ef0a6edaac0af5a9a146a1c
uakdogan <uakdogan@mycompany.com>
2013-02-25 19:57:12 +0000

    added rel="nofollow" to user action links on job details page
    git-svn-id: file:///svn2git/svn/repos/emarket/website/trunk@519 5bad16f7-618a-4fe3-8011-2f6d34f883f8

--- apps/camp/modules/jobs/templates/_leftmenu_company.php
+++ apps/camp/modules/jobs/templates/_leftmenu_company.php
@@ -2,7 +2,7 @@
         <div class="box_180 noBorder">
             <ul class="_comMenu">
                 <li class="_profile<?php echo $action == 'profile' ? ' selected' : '' ?>"><?php echo link_to('<span></span>'.__('Company Profile'), $company->getProfileUrl()) ?></li>
-                <li class="_save<?php echo $saved = $sesuser->getUserCompany($company->getId(), UserBookmarkPeer::BMTYP_FAVOURITE) ? ' click' : '' ?>"><?php echo link_to('<span></span>'.($saved ? __('Remove Bookmark') : __('Bookmark Company')), "@company-jobs?hash={$company->getHash()}&act=".($saved ? 'rem' : 'save')) ?></li>
-                <li class="_block<?php echo $blocked = $sesuser->getUserCompany($company->getId(), UserBookmarkPeer::BMTYP_BANNED) ? ' click' : '' ?>"><?php echo link_to('<span></span>'.($blocked ? _('Unblock Company') : __('Block Company')), "@company-jobs?hash={$company->getHash()}&act=".($blocked ? 'unb' : 'ban')) ?></li>
+                <li class="_save<?php echo $saved = $sesuser->getUserCompany($company->getId(), UserBookmarkPeer::BMTYP_FAVOURITE) ? ' click' : '' ?>"><?php echo link_to('<span></span>'.($saved ? __('Remove Bookmark') : __('Bookmark Company')), "@company-jobs?hash={$company->getHash()}&act=".($saved ? 'rem' : 'save'), 'rel=nofollow') ?></li>
+                <li class="_block<?php echo $blocked = $sesuser->getUserCompany($company->getId(), UserBookmarkPeer::BMTYP_BANNED) ? ' click' : '' ?>"><?php echo link_to('<span></span>'.($blocked ? _('Unblock Company') : __('Block Company')), "@company-jobs?hash={$company->getHash()}&act=".($blocked ? 'unb' : 'ban'), 'rel=nofollow') ?></li>
             </ul>
         </div>
--- apps/camp/modules/jobs/templates/_leftmenu_group.php
+++ apps/camp/modules/jobs/templates/_leftmenu_group.php
@@ -2,7 +2,7 @@
         <div class="box_180 noBorder">
             <ul class="_comMenu">
                 <li class="_profile<?php echo $action == 'profile' ? ' selected' : '' ?>"><?php echo link_to('<span></span>'.__('Group Profile'), $group->getProfileUrl()) ?></li>
-                <li class="_save<?php echo $saved = $sesuser->getUserGroup($group->getId(), UserBookmarkPeer::BMTYP_FAVOURITE) ? ' click' : '' ?>"><?php echo link_to('<span></span>'.($saved ? __('Remove Bookmark') : __('Bookmark Group')), $group->getProfileActionUrl('jobs')."&act=".($saved ? 'rem' : 'save')) ?></li>
-                <li class="_block<?php echo $blocked = $sesuser->getUserGroup($group->getId(), UserBookmarkPeer::BMTYP_BANNED) ? ' click' : '' ?>"><?php echo link_to('<span></span>'.($blocked ? _('Unblock Group') : __('Block Group')), $group->getProfileActionUrl('jobs')."&act=".($blocked ? 'unb' : 'ban')) ?></li>
+                <li class="_save<?php echo $saved = $sesuser->getUserGroup($group->getId(), UserBookmarkPeer::BMTYP_FAVOURITE) ? ' click' : '' ?>"><?php echo link_to('<span></span>'.($saved ? __('Remove Bookmark') : __('Bookmark Group')), $group->getProfileActionUrl('jobs')."&act=".($saved ? 'rem' : 'save'), 'rel=nofollow') ?></li>
+                <li class="_block<?php echo $blocked = $sesuser->getUserGroup($group->getId(), UserBookmarkPeer::BMTYP_BANNED) ? ' click' : '' ?>"><?php echo link_to('<span></span>'.($blocked ? _('Unblock Group') : __('Block Group')), $group->getProfileActionUrl('jobs')."&act=".($blocked ? 'unb' : 'ban'), 'rel=nofollow') ?></li>
             </ul>
         </div>
