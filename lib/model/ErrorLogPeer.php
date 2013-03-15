d07f0ea1db9a67227a9eb3b7083809e6f554bb93
uakdogan <uakdogan@mycompany.com>
2013-03-15 12:40:57 +0000

    added new app, module, action name information to error log data
    git-svn-id: file:///svn2git/svn/repos/emarket/website/trunk@566 5bad16f7-618a-4fe3-8011-2f6d34f883f8

--- lib/model/ErrorLogPeer.php
+++ lib/model/ErrorLogPeer.php
@@ -4,6 +4,7 @@ class ErrorLogPeer extends BaseErrorLogPeer
 {
     public static function Log($item_id, $item_type_id, $message, $e = null)
     {
+        $context = sfContext::getInstance();
         $stack = $e ? $e->outputStackTrace() : '[object absent]';
         $message .= "\n\nStack:\n{$stack}";
         try
@@ -12,8 +13,11 @@ class ErrorLogPeer extends BaseErrorLogPeer
             $log->setCausingItemId($item_id ? $item_id : 0);
             $log->setCausingItemTypeId($item_type_id ? $item_type_id : 0);
             $log->setMessage($message);
+            $log->setApplication($context->getConfiguration()->getApplication());
+            $log->setModule($context->getModuleName());
+            $log->setAction($context->getActionName());
             $log->save();
-            mail('ukbe.akdogan@emarketturkey.com', 'Error occured', $e ? print_r($e, true) : $message);
+            mail('ukbe.akdogan@emarketturkey.com', 'Error occured', $e ? print_r($e, true) : "$message\n\nApplication:{$log->getApplication()}\nModule:{$log->getModule()}\nAction:{$log->getAction()}");
         }
         catch (Exception $e)
         {
