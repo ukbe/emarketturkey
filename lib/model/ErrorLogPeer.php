89503b51451cc9e76e246a5e95214eeeede5db10
uakdogan <uakdogan@mycompany.com>
2013-03-15 14:03:32 +0000

    bugfix
    git-svn-id: file:///svn2git/svn/repos/emarket/website/trunk@569 5bad16f7-618a-4fe3-8011-2f6d34f883f8

--- lib/model/ErrorLogPeer.php
+++ lib/model/ErrorLogPeer.php
@@ -6,18 +6,18 @@ class ErrorLogPeer extends BaseErrorLogPeer
     {
         $context = sfContext::getInstance();
         $stack = $e ? $e->outputStackTrace() : '[object absent]';
-        if ($e) $message .= (!is_null($message) ? "\"" . $message . "\"\n" : '') . "Message: {$e->getMessage()}\nFile: {$e->getFile()}\nLine: {$e->getLine()}\n\nApplication:{$log->getApplication()}\nModule:{$log->getModule()}\nAction:{$log->getAction()}\n\nStack:\n{$stack}";
-        $message .= "";
         try
         {
             $log = new ErrorLog();
             $log->setCausingItemId($item_id ? $item_id : 0);
             $log->setCausingItemTypeId($item_type_id ? $item_type_id : 0);
-            $log->setMessage($message);
             $log->setApplication($context->getConfiguration()->getApplication());
-            $log->setModule($context->getModuleName());
-            $log->setAction($context->getActionName());
+            $log->setModuleName($context->getModuleName());
+            $log->setActionName($context->getActionName());
+            if ($e) $message .= (!is_null($message) ? "\"" . $message . "\"\n" : '') . "Message: {$e->getMessage()}\nFile: {$e->getFile()}\nLine: {$e->getLine()}\n\nApplication:{$log->getApplication()}\nModule:{$log->getModuleName()}\nAction:{$log->getActionName()}\n\nStack:\n{$stack}";
+            $log->setMessage($message);
             $log->save();
+
             mail('ukbe.akdogan@emarketturkey.com', 'Error occured', $message);
         }
         catch (Exception $e)
