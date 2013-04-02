e7dfd894c33646379bd3237e828d49e46ed82f94
uakdogan <uakdogan@mycompany.com>
2013-04-02 10:11:32 +0000

    added TOSİAD campaign files
    git-svn-id: file:///svn2git/svn/repos/emarket/website/trunk@573 5bad16f7-618a-4fe3-8011-2f6d34f883f8

---
+++ apps/camp/modules/services/templates/_campaign_tosiad.spr2013_custom.php
@@ -0,0 +1,5 @@
+                            <dl class="_table campaign-form">
+                                <dt><?php echo emt_label_for('Campaign', __('Campaign')) ?></dt>
+                                <dd class="margin-b2 t_bold t_blue t_large _noInput"><?php echo $campaign->getDisplayName() ?></dd>
+                                </dd>
+                            </dl>
